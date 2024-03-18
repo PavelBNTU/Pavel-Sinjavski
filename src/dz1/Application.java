536479fa0a1f3f5132c8d316d26fbad38108f49d
Павел <pavel_sinjavski_pavel@mail.ru>
2024-03-18 20:53:13 +0300

    Примитивы на практике

---
+++ src/dz1/Application.java
@@ -0,0 +1,31 @@
+package dz1;
+
+public class Application {
+    public static void main(String[] args) {
+        byte footSize = 42;
+        System.out.println(footSize);
+
+        footSize = 44;
+
+        short age = 58;
+        System.out.println(age);
+
+        int salary = 1_000_000;
+        System.out.println(salary);
+
+        long topManagerSalary = 4_887_813L;
+        System.out.println(topManagerSalary);
+
+        float temperature = 36.6f;
+        System.out.println(temperature);
+
+        double temperatureDouble = 36.6;
+        System.out.println(temperatureDouble);
+
+        boolean student = true;
+        System.out.println(student);
+
+        System.out.println(footSize);
+
+    }
+}
