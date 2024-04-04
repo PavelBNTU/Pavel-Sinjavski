99376455eb8518da6530c0de81e8588f4be32c8b
Павел <pavel_sinjavski_pavel@mail.ru>
2024-04-04 22:56:46 +0300

    переделал код

--- src/dz6/Example.java
+++ src/dz6/Example.java
@@ -6,19 +6,19 @@ import java.util.regex.Pattern;
 import static java.awt.SystemColor.text;
 
 public class Example {
-    public static void main(String[] args) {
-        final String regex = "1234567|[A-z]";
-        final String string = "Мой номер телефона +375-29-1234567. Мой ник в Телеграм @TheGodFather.";
-
-        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
-        final Matcher matcher = pattern.matcher(string);
-
-        while (matcher.find()) {
-            System.out.println("Полное соответствие: " + matcher.group(0));
-
-            for (int i = 1; i <= matcher.groupCount(); i++) {
-                System.out.println("Группа " + i + ": " + matcher.group(i));
-            }
+    public static void main(String[] args){
+    String text = "Мой номер телефона +375-29-1234567. Мой ник в Телеграм @TheGodFather.";
+        Pattern phonePattern = Pattern.compile("\\d{7}");
+        Matcher phoneMatcher = phonePattern.matcher(text);
+        if (phoneMatcher.find()) {
+            String phoneNumber = phoneMatcher.group();
+            System.out.println("Номер телефона : " + phoneNumber);
+        }
+        Pattern telegramPattern = Pattern.compile("(?<=@)[A-Za-z0-9_]+");
+        Matcher telegramMatcher = telegramPattern.matcher(text);
+        if (telegramMatcher.find()) {
+            String telegramNick = telegramMatcher.group();
+            System.out.println("Ник в Телеграм : " + telegramNick);
         }
     }
 }
